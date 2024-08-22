675ab3c9d6c2febc6c8b21fd5d763552b1a3a550
davidfernndez775 <davidfernndez775@yahoo.com>
2024-08-22 09:20:01 -0400

    form create and update

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Hero, Publisher } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
   styles: ``,
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit {
   // definimos un formulario con varios campos
   public heroForm = new FormGroup({
     // por defecto Angular asume el tipo de dato de cada campo
@@ -27,7 +29,11 @@ export class NewPageComponent {
   ];
 
   // injectamos el servicio que va a recibir el formulario
-  constructor(private heroesService: HeroesService) {}
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
 
   // tomamos los datos del formulario
   get currentHero(): Hero {
@@ -37,9 +43,47 @@ export class NewPageComponent {
     return hero;
   }
 
+  // tanto la pagina de creacion como la de actualizacion comparten la misma page
+  // por tanto es necesario saber cuando el formulario va a aparecer vacio
+  // para un create o con los datos de un heroe para un update
+  ngOnInit(): void {
+    // si la direccion no incluye el termino 'edit' vamos a hacer una creacion
+    if (!this.router.url.includes('edit')) return;
+
+    // si lo anterior no ocurre vamos a realizar un update por tanto hay que
+    // cargar los datos del heroe en el formulario
+    this.activatedRoute.params
+      .pipe(
+        // el switchMap
+        switchMap(({ id }) => this.heroesService.getHeroesById(id))
+      )
+      .subscribe((hero) => {
+        // si no existe un heroe retornamos a la pagina principal
+        if (!hero) return this.router.navigateByUrl('/');
+        // si existe un heroe usamos la funcion reset para que tome sus valores
+        this.heroForm.reset(hero);
+        return;
+      });
+  }
+
   // definimos la funcion que controla el formulario
   onSubmit(): void {
     // si el formulario es invalido no pasa nada
     if (this.heroForm.invalid) return;
+
+    // si los datos del formulario contienen un id se hace un update
+    if (this.currentHero.id) {
+      // llamamos a la funcion del servicio y nos suscribimos al observable
+      this.heroesService.updateHero(this.currentHero).subscribe((hero) => {
+        // todo: mostrar snackbar
+      });
+      return;
+    }
+
+    // si no se ejecutan ninguno de los if anteriores significa que tenemos
+    // un formulario valido sin un id entre sus datos por tanto se hace un create
+    this.heroesService.addHero(this.currentHero).subscribe((hero) => {
+      // todo: mostrar snackbar y redirigir a heroes/edit heroes.id
+    });
   }
 }
